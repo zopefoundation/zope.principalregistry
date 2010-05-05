29208903c47420b5063d1dc298de87d6f70e0d63
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:12:24 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2001-2009 Zope Corporation and Contributors.
+# Copyright (c) 2001-2009 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(name='zope.principalregistry',
       version = '3.7.1',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Global principal registry component for Zope3',
       long_description=(
