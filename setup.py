737abf812680ae6c53ac2bc431e81ca7df3f6de3
Michael Howitz <icemac@gmx.net>
2025-02-14 11:28:57 +0100

    Preparing release 5.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.principalregistry',
-      version='5.1.dev0',
+      version='5.1',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Global principal registry component for Zope3',
