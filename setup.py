9c9210ba535f05f01d6e6aa6555dcb9065b6ede1
Michael Howitz <icemac@gmx.net>
2025-07-03 09:29:43 +0200

    Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@
 """
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -26,7 +25,7 @@ def read(*rnames):
 
 TESTS_REQUIRE = [
     'zope.testing',
-    'zope.testrunner',
+    'zope.testrunner >= 6.4',
 ]
 
 setup(name='zope.principalregistry',
@@ -61,9 +60,6 @@ setup(name='zope.principalregistry',
       ],
       url='https://github.com/zopefoundation/zope.principalregistry',
       license='ZPL-2.1',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['zope'],
       python_requires='>=3.9',
       install_requires=[
           'setuptools',
